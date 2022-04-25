3613596829be3e9d200dcafdcd09bfb3a1308f95
João Flávio Santos <joaoflavio.s@hotmail.com>
2022-04-25 12:39:51 -0300

    feat: Implementing v-if, v-else and v-show in InfoComponent

--- vue3-vite/src/components/InfoComponent.vue
+++ vue3-vite/src/components/InfoComponent.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
-    <p>I'm working at this moment.</p>
+    <p v-if="working">I'm working at this moment.</p>
+    <p v-else>I'm looking for new opportunities.</p>
     <p>I'm using this technologies:</p>
     <ul>
       <li>Javascript</li>
       <li>Python</li>
       <li>PHP</li>
     </ul>
+    <p v-show="show_email">Send a message to: {{ email }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: "InfoComponent",
+  data() {
+    return {
+      working: false,
+      show_email: true,
+      email: "jhon@gmail.com",
+    };
+  },
 };
 </script>
