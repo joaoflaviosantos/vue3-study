8f381a62c89743b439adfabe8d54fd3dc6feb14b
João Flávio Santos <joaoflavio.s@hotmail.com>
2022-04-29 21:50:47 -0300

    feat: Implementing $emit from PictureChange (child) to PictureComponent (father)

---
+++ vue3-vite/src/components/PictureChange.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <button @click="$emit('changePicture')">Change profile picture</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PictureChange",
+  emits: ["changePicture"],
+};
+</script>
--- vue3-vite/src/components/PictureComponent.vue
+++ vue3-vite/src/components/PictureComponent.vue
@@ -1,17 +1,31 @@
 <template>
   <div>
-    <img :src="avatar" :alt="description" height="200"/>
+    <img :src="avatar" :alt="description" height="200" />
+    <PictureChange @change-picture="changePicture" />
   </div>
 </template>
 
 <script>
+import PictureChange from "./PictureChange.vue";
 export default {
   name: "PictureComponent",
   data() {
     return {
+      avatar_standard: "/img/my_avatar.png",
       avatar: "/img/my_avatar.png",
+      avatar_2: "/img/my_avatar_2.png",
       description: "John",
     };
   },
+  methods: {
+    changePicture() {
+      if (this.avatar == this.avatar_standard) {
+        this.avatar = this.avatar_2;
+      } else {
+        this.avatar = this.avatar_standard;
+      }
+    },
+  },
+  components: { PictureChange },
 };
 </script>
